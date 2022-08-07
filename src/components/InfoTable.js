d40d42536a89f575d79f97d055d457e24f904bec
Prateek <prateekdang54@gmail.com>
2022-08-08 01:31:09 +0530

    update to web3

--- src/components/InfoTable.js
+++ src/components/InfoTable.js
@@ -6,24 +6,37 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-
+import { getPoolInfo } from './connection';
 
 function createData(name, calories, fat, carbs, protein) {
   return { name, calories, fat, carbs, protein };
 }
 
-const rows = [
-  createData('Token Name', 'Bitcoin' ),
-  createData('Address', '0x0000000000000000000000000'),
-  createData('Total Supply', 1000000),
-  createData('Tokens/USD', 305),
-  createData('USD/Token', 1),
-  createData('Buy Tax',5),
-  createData('Sell Tax',5)
-];
 
-export default function DenseTable() {
+
+export default function DenseTable(props) {
+  var data ={Address:null,
+    token2USD: null,
+    USD2token: null,
+    buyTax: null,
+    saleTax: null};
+
+  React.useEffect(()=>{
+    console.log(props.poolInfo)
+    data = props.poolInfo
+  })
+  
+  const rows = [
+    createData('Token Name', 'Bitcoin' ),
+    createData('Address', data.Address),
+    createData('Total Supply', ''),
+    createData('Tokens/USD', data.token2USD),
+    createData('USD/Token', data.USD2token),
+    createData('Buy Tax',data.buyTax),
+    createData('Sell Tax',data.saleTax)
+  ];
   return (
+
 
     <TableContainer component={Paper}>
       <Table sx={{ maxWidth: "100%" }} size="small" aria-label="Token-Information" id="Token-Information">
